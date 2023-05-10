0b0fb0e718529c96852532bdc5d1035a386203e2
Reem Ibrahim <118621764+Rti0@users.noreply.github.com>
2023-05-10 21:17:58 +0300

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,19 @@
+public class Main1 {
+
+
+    public static void main(String[] args) {
+        MovablePoint m= new MovablePoint(3,4,-3,5);
+
+        m.moveUp();
+        m.moveDown();
+        m.moveLeft();
+        m.moveRight();
+
+        MovablePoint R=new MovablePoint(1,3,5,2);
+
+        R.moveUp();
+        R.moveDown();
+        R.moveLeft();
+        R.moveRight();
+    }
+}
