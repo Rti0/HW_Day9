c993a124a20ddbada0cfa84708cd6b1865b3d76c
Reem Ibrahim <118621764+Rti0@users.noreply.github.com>
2023-05-10 21:12:38 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-      Book B1=new Book("chaos theory",40.0,"Edwared Lorenz") ;
-      B1.displayInfo();
-      Movie M1= new Movie("Titanic",50.0,"James Cameron");
-      M1.displayInfo();
 
+
+    public static void main(String[] args) {
+        MovablePoint m= new MovablePoint(3,4,-3,5);
+
+        m.moveUp();
+        m.moveDown();
+        m.moveLeft();
+        m.moveRight();
+
+        MovablePoint R=new MovablePoint(1,3,5,2);
+
+        R.moveUp();
+        R.moveDown();
+        R.moveLeft();
+        R.moveRight();
     }
 }
